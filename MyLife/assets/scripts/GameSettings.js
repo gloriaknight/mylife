364b5a9fb2c1ba01c4962bbdbf22596ac46d6c65
Cronus <gloriaknight@163.com>
2017-10-10 23:51:54 +0800

    增加游戏设置页面的翻页功能

--- MyLife/assets/scripts/GameSettings.js
+++ MyLife/assets/scripts/GameSettings.js
@@ -3,21 +3,31 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        pageNum: 4,
+        pageTemplate: cc.Prefab,
+        target: cc.PageView,
+        label: cc.Label
     },
 
     // use this for initialization
     onLoad: function () {
         cc.info("player's name is " + gameData.userData.playerInfo.playerName);
+    },
+
+    jumpToStatusSettings(){
+        this.target.scrollToPage(0);
+    },
+
+    jumpToSkillSettings(){
+        this.target.scrollToPage(1);
+    },
+
+    jumpToKnowledgeSettings(){
+        this.target.scrollToPage(2);
+    },
+
+    jumpToSocialSettings(){
+        this.target.scrollToPage(3);
     },
 
     // called every frame, uncomment this function to activate update callback
